3b479de13090b6eff296df8a38307fd5a950727f
Joaquín Aniorte <dreammaker@gmail.com>
2021-02-11 23:52:37 +0100

    End of section 7

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import { Button, Container, Menu } from 'semantic-ui-react'
+import { observer } from "mobx-react-lite";
+import React, { useContext } from "react";
+import { Button, Container, Menu } from "semantic-ui-react";
+import ActivityStore from "../../app/stores/activityStore";
 
-interface IProps {
-    openCreateForm: () => void;
-}
+const NavBar: React.FC = () => {
+  const activityStore = useContext(ActivityStore);
+  return (
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item header>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="Activities" />
+        <Menu.Item>
+          <Button
+            onClick={activityStore.openCreateForm}
+            positive
+            content="Create Activity"
+          />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
 
-export const NavBar: React.FC<IProps> = ({openCreateForm}) => {
-    return (
-        <Menu fixed='top' inverted>
-            <Container>
-                <Menu.Item header>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
-                    Reactivities
-                </Menu.Item>
-                <Menu.Item name='Activities' />
-                <Menu.Item>
-                    <Button onClick={openCreateForm} positive content='Create Activity'/>
-                </Menu.Item>
-            </Container>
-      </Menu>
-    )
-}
+export default observer(NavBar);
