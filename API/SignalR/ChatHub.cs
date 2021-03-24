a4faf08b713a6c8b341d05180ce9ea639b1830ef
Joaquín Aniorte <dreammaker@gmail.com>
2021-03-24 07:44:19 +0100

    End of section 19

---
+++ API/SignalR/ChatHub.cs
@@ -0,0 +1,47 @@
+﻿using Application.Comments;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace API.SignalR
+{
+    public class ChatHub : Hub
+    {
+        private readonly IMediator Mediator;
+
+        public ChatHub(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+
+        public async Task SendComment(Create.Command command)
+        {
+            command.Username = GetUsername();
+
+            var comment = await Mediator.Send(command);
+
+            await Clients.Group(command.ActivityId.ToString()).SendAsync("ReceiveComment", comment);
+        }
+
+        public string GetUsername()
+        {
+            return (Context.User?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{GetUsername()} has joined the group");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{GetUsername()} has left the group");
+        }
+    }
+}
